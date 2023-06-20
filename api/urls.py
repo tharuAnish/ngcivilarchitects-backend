1b9fc81960cbf8cf249c3072ae6ee88e7697ec38
tharuAnish <namoanishtharu@gmail.com>
2023-06-21 00:58:44 +0530

    blogs created and added

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from .views import *
+from . import views
 
 urlpatterns = [
     path('', home),
@@ -8,5 +9,9 @@ urlpatterns = [
     path("api/testimonials/", testimonialApiView.as_view()),
     path("api/team/", teamApiView.as_view()),
     path("api/project/", projectApiView.as_view()),
+    path("api/blog/", blogApiView.as_view()),
+    # Endpoint for fetching a specific blog by ID
+    path("api/blog/<int:blog_id>/", blogApiView.as_view(), name="blog-detail"),
+
   
 ]
